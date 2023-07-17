a874827f3ba28e34c4a5b1f41724517c54b848f9
Octal <oliver@olivermakesco.de>
2023-07-17 17:09:13 -0500

    guh

---
+++ src/main/java/dev/sweetberry/liberry/LiberryMod.java
@@ -0,0 +1,9 @@
+package dev.sweetberry.liberry;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+
+public class LiberryMod implements ModInitializer {
+	@Override
+	public void onInitialize(ModContainer mod) {}
+}
---
+++ src/main/java/dev/sweetberry/liberry/config/ConfigHelper.java
@@ -0,0 +1,11 @@
+package dev.sweetberry.liberry.config;
+
+import net.minecraft.util.Identifier;
+import org.quiltmc.config.api.ReflectiveConfig;
+import org.quiltmc.loader.api.config.v2.QuiltConfig;
+
+public class ConfigHelper {
+	public static <C extends ReflectiveConfig> C create(Identifier id, Class<C> clazz) {
+		return QuiltConfig.create(id.getNamespace(), id.getPath(), clazz);
+	}
+}
